409688eb090125bf1ee8181eadc459017e38b1b4
FDIL <fdil.fadilh1@gmail.com>
2022-11-05 10:24:37 -0600

    Update test.c and test.h for new testing
    Now trying to test displaying of the bar with a number, then the new line.

--- Fadil_ADC_Test/test.c
+++ Fadil_ADC_Test/test.c
@@ -1,3 +1,5 @@
+#include "test.h"
+
 uint16_t sample_voltage;
 
 void test()
@@ -5,8 +7,23 @@ void test()
     while(1)
     {
         sample_voltage = do_ADC();
+        displayBarPercent(sample_voltage);
+        //displayBar16Box(sample_voltage); //alternative option of bar
         Disp2Hex(sample_voltage);
-        Disp2String('\n');
-        // Only 
+        XmitUART2('\n', 1);
     }
+}
+
+void displayBarPercent(uint16_t voltage)
+{
+    // First calculate percent
+    unsigned int percent = voltage/1023*100;
+    XmitUART2(219, percent);
+}
+
+void displayBar16Box(uint16_t voltage)
+{
+    // First divide voltage into 16 parts
+    unsigned int percent = voltage/1023*16;
+    XmitUART2(219, percent);
 }
--- Fadil_ADC_Test/test.h
+++ Fadil_ADC_Test/test.h
@@ -13,5 +13,22 @@ void test();
 //displays the converted signal into a terminal
 // via sending it through UART-USB
 
+void displayBarPercent(uint16_t voltage);
+//REQUIRES:
+// voltage to be a number part of the set [0, 1023]
+//PROMISES:
+// Displays onto the terminal the number of boxes as the number of percent.
+// Percent calculated out of 1023.
+// Each box on terminal = 1%.
+// Any decimal values gets rounded down.
+
+void displayBar16Box(uint16_t voltage);
+//REQUIRES:
+// voltage to be a number part of the set [0, 1023]
+//PROMISES:
+// Displays onto the terminal a bar, with 16 boxes at maximum.
+// Each box is worth 64.
+// Any voltage between multiples of 64 are rounded down to lower multiple of 64.
+
 #endif	/* TEST_H */
 
